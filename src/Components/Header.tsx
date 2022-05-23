2c584d2481948a16769ab42c9ede6792ab9391c9
iWDNN <cksgh2064@daum.net>
2022-05-23 16:36:45 +0900

    NOMFLIX - header part four - useAnimation, useViewportScroll

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import { motion, Variants } from "framer-motion";
+import { motion, Variants, useAnimation, useViewportScroll } from "framer-motion";
 import { Link, useMatch, PathMatch } from "react-router-dom";
-import { useLayoutEffect, useRef, useState } from "react";
-const Nav = styled.nav`
+import { useEffect, useState } from "react";
+const Nav = styled(motion.nav)`
   box-sizing: border-box;
   display: flex;
   justify-content: space-between;
@@ -10,7 +10,6 @@ const Nav = styled.nav`
   position: fixed;
   width: 100%;
   top: 0;
-  background-color: black;
   height: 68px;
   font-size: 12px;
   padding: 0 59px;
@@ -60,36 +59,30 @@ const UnderLine = styled(motion.div)`
   background-color: ${(props) => props.theme.red};
 `;
 
-const SearchBox = styled.div`
-  display: flex;
-  align-items: center;
+const Search = styled.span`
   color: white;
-`;
-
-const SearchIcon = styled.svg`
-  width: 20px;
-  height: 20px;
-  padding: 0 6px;
-`;
-
-const InputBox = styled(motion.div)`
   display: flex;
   align-items: center;
-  border: 1px solid white;
-  padding: 5px;
-  transform-origin: right center;
-  input {
-    width: 200px;
-    background-color: rgba(0, 0, 0, 0.7);
-    outline: none;
-    color: white;
-    border: none;
-    padding: 5px 0;
-    &::placeholder {
-      color: rgba(255, 255, 255, 0.6);
-    }
-  }
+  position: relative;
 `;
+const SearchIcon = styled(motion.svg)`
+  position:absolute;
+  left : 10px;
+  height:25px;
+`
+
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right:0;
+  z-index: -1;
+  background-color: black;
+  outline:none;
+  border:1px solid white;
+  padding: 7px 0 7px 35px;
+  color:white;
+`;
+
 
 const svgVar: Variants = {
   start: {
@@ -111,16 +104,53 @@ const svgVar: Variants = {
   },
 };
 
+const navVar: Variants = {
+  top: {
+    backgroundColor: "rgba(0,0,0,0)"
+  },
+  scroll: {
+    backgroundColor: "rgba(0,0,0,1)",
+  }
+}
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
+  const inputAnimation = useAnimation();
+  const navAnimation = useAnimation();
+  const { scrollY } = useViewportScroll();
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const toggleSearch = () => {
+    if (searchOpen) {
+      inputAnimation.start({
+        scaleX: 0
+      })
+    } else {
+      inputAnimation.start({
+        scaleX: 1
+      })
+    }
+    setSearchOpen(prev => !prev)
+  }
+  useEffect(() => {
+    scrollY.onChange(() => {
+      if (scrollY.get() > 80) {
+        navAnimation.start("scroll")
+      } else {
+        navAnimation.start("top")
+      }
+    })
+  }, [scrollY, navAnimation])
+
 
   const homeMatch: PathMatch<string> | null = useMatch("/");
   const tvMatch: PathMatch<string> | null = useMatch("/tv");
 
   return (
-    <Nav>
+    <Nav
+      variants={navVar}
+      animate={navAnimation}
+      initial="top"
+    >
       <Col>
         <Logo xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 276.742">
           <motion.path
@@ -148,36 +178,30 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <SearchBox
-          onBlur={() => setSearchOpen((prev) => !prev)}
-          onClick={async () => {
-            await setSearchOpen(true);
-            if (inputRef.current !== null) inputRef.current.focus();
-          }}
-        >
-          {searchOpen ? (
-            <InputBox initial={{ scaleX: 0 }} animate={{ scaleX: 1 }}>
-              <SearchIcon
-                fill="white"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
-              </SearchIcon>
-              <input ref={inputRef} placeholder="제목, 사람, 장르" />
-            </InputBox>
-          ) : (
-            <SearchIcon
-              fill="white"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
-            </SearchIcon>
-          )}
-        </SearchBox>
+        <Search>
+          <SearchIcon
+            onClick={toggleSearch}
+            animate={{ x: searchOpen ? -180 : 0 }}
+            transition={{ type: "linear" }}
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clipRule="evenodd"
+            ></path>
+          </SearchIcon>
+          <Input
+            animate={inputAnimation}
+            initial={{ scaleX: 0 }}
+            transition={{ type: "linear" }}
+            placeholder="제목, 사람, 장르"
+          />
+        </Search>
       </Col>
-    </Nav>
+    </Nav >
   );
 }
 export default Header;
